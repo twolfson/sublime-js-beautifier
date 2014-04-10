6f4838e334d4882aa87c49059181bb6284462851
Todd Wolfson <todd@twolfson.com>
2014-04-09 22:43:37 -0700

    Completed beautify invocation logic

--- js-beautifier.py
+++ js-beautifier.py
@@ -7,14 +7,19 @@ class JsBeautifierCommand(sublime_plugin.TextCommand):
         """Run JS Beautifer CLI scripts against current view"""
         # Load the contents of the view
         view = self.view
-        content = view.substr(sublime.Region(0, view.size()))
+        all_text = sublime.Region(0, view.size())
+        content = view.substr(all_text)
 
         # TODO: Deteremine if the view is JS, CSS, or HTML
         # https://github.com/einars/js-beautify/blob/v1.4.2/index.html#L245-L269
 
         # Invoke `js-beautifier` CLI
         child = subprocess.Popen(['js-beautify', '--file', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        child.communicate(input=content)
-        # child.terminate()
-        # child.wait()
-        print child.stdout
+        child.stdin.write(content)
+        child.stdin.close()
+        child.wait()
+        beautified_content = child.stdout.read()
+
+        # Overwrite the current content
+        view.replace(edit, all_text, beautified_content)
+        view.end_edit(edit)
