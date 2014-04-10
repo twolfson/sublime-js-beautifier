607194c6aac83c43f322ff310b33b387cf35e588
Todd Wolfson <todd@twolfson.com>
2014-04-09 22:32:08 -0700

    Fixed up command runner

--- js-beautifier.py
+++ js-beautifier.py
@@ -2,8 +2,8 @@ import subprocess
 import sublime_plugin
 import sublime
 
-class JsBeautifierCommand(sublime_plugin.WindowCommand):
-    def run(self):
+class JsBeautifierCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
         """Run JS Beautifer CLI scripts against current view"""
         # Load the contents of the view
         view = self.view
@@ -13,4 +13,3 @@ class JsBeautifierCommand(sublime_plugin.WindowCommand):
         # TODO: Deteremine if the view is JS, CSS, or HTML
 
         # Invoke `js-beautifier` CLI
-
